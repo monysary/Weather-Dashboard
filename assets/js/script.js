704a493fcf925d675b21172d7ceec3e5badcd298
monysary <sary.mony@gmail.com>
2022-11-19 17:19:52 -0800

    Added fetch API for current weather and forecast weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-var APIkey = "59bb2d1c760fcf9313acc25aca14f9ce"
+var APIkey = "59bb2d1c760fcf9313acc25aca14f9ce";
+var cityName = "Los Angeles";
+var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey + "&units=imperial";
+var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIkey + "&units=imperial";
+
+fetch(currentWeatherUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+
+fetch(forecastUrl)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+})
