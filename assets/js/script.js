379c6e9c94688e35627a1202faa49e0793bbbf4d
monysary <sary.mony@gmail.com>
2022-11-21 18:36:11 -0800

    Added forecast dates to forecast cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,7 @@ var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityNam
 // Variables for pulling data from API
 var iconID;
 var weatherIconUrl;
-var unixTimestamp
-var dateArr = Date(unixTimestamp).split(" ");
+var dateArr = Date().split(" ");
 var day = dateArr[0];
 var month = dateArr[1];
 var date = dateArr[2];
@@ -20,23 +19,22 @@ fetch(currentWeatherUrl)
         return response.json();
     })
     .then(function(data) {
-        console.log(data);
-        // Fetch current city
-        document.querySelector("#city-name").textContent = data.name
+        // Fetch city name
+        document.querySelector("#city-name").textContent = data.name;
         
-        // Fetch current date
-        unixTimestamp = data.dt
-        document.querySelector("#current-date").textContent = day + ", " + month + " " + date + ", " + year
+        // Display current date
+        dateArr = Date().split(" ");
+        document.querySelector("#current-date").textContent = day + ", " + month + " " + date + ", " + year;
 
         // Fetch current weather icon
-        iconID = data.weather[0].icon
+        iconID = data.weather[0].icon;
         weatherIconUrl = "http://openweathermap.org/img/wn/" + iconID + "@2x.png";
-        document.querySelector("#day0img").setAttribute("src", weatherIconUrl)
+        document.querySelector("#day0img").setAttribute("src", weatherIconUrl);
 
         // Fetch temperature, humidity, and wind data
-        document.querySelector("#temperature").textContent = data.main.temp
-        document.querySelector("#humidity").textContent = data.main.humidity
-        document.querySelector("#wind").textContent = data.wind.speed
+        document.querySelector("#temperature").textContent = data.main.temp;
+        document.querySelector("#humidity").textContent = data.main.humidity;
+        document.querySelector("#wind").textContent = data.wind.speed;
     })
 
 // Fetch 5-day forecast weather data
@@ -45,5 +43,19 @@ fetch(forecastUrl)
     return response.json();
 })
 .then(function(data) {
-    // console.log(data);
+    console.log(data);
+
+    // Fetch forecast date
+    var d1Date = data.list[3].dt_txt.split(" ")[0];
+    document.querySelector("#day1forecast").textContent = d1Date
+    var d2Date = data.list[11].dt_txt.split(" ")[0];
+    document.querySelector("#day2forecast").textContent = d2Date
+    var d3Date = data.list[19].dt_txt.split(" ")[0];
+    document.querySelector("#day3forecast").textContent = d3Date
+    var d4Date = data.list[27].dt_txt.split(" ")[0];
+    document.querySelector("#day4forecast").textContent = d4Date
+    var d5Date = data.list[35].dt_txt.split(" ")[0];
+    document.querySelector("#day5forecast").textContent = d5Date
+    
+    
 })
